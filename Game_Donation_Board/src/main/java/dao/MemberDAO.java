c6d30cad4b2ce6007682a208498788804d32437d
lee jun seong <kimlsy2444@naver.com>
2024-02-23 15:34:17 +0900

    정리중

--- Game_Donation_Board/src/main/java/dao/MemberDAO.java
+++ Game_Donation_Board/src/main/java/dao/MemberDAO.java
@@ -2,7 +2,7 @@ package dao;
 
 import org.apache.ibatis.session.SqlSession;
 
-import dto.MemberDTO;
+import dto.EF_UserDTO;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -10,12 +10,12 @@ public class MemberDAO {
 	
 	final SqlSession sqlSession;
 
-	public  MemberDTO loginCheck(String id) {
+	public  EF_UserDTO loginCheck(String id) {
 		
 		return sqlSession.selectOne("m.loginCheck",id);
 	}
 
-	public int member_insert(MemberDTO dto) {
+	public int member_insert(EF_UserDTO dto) {
 		
 		
 		return sqlSession.insert("m.insert",dto);
