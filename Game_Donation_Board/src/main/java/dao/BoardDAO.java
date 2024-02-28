3046e1e6390bb7be2717d59734f78c4a3c5a5435
lee jun seong <111180369+junseong12338@users.noreply.github.com>
2024-02-28 21:17:20 +0900

    Revert "Squashed commit of the following:"
    This reverts commit fd0e463444298b0ce2f0a4722ada0ad84916bff6.

--- Game_Donation_Board/src/main/java/dao/BoardDAO.java
+++ Game_Donation_Board/src/main/java/dao/BoardDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.apache.ibatis.session.SqlSession;
 
 import dto.BoardDTO;
-import dto.UserDTO;
+import dto.MemberDTO;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
--- Game_Donation_Board/target/classes/context/Context_1_mybatis.java
+++ Game_Donation_Board/target/classes/context/Context_1_mybatis.java
@@ -1,5 +1,6 @@
 package context;
 
+
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -12,16 +13,12 @@ import org.springframework.core.io.ClassPathResource;
 import oracle.jdbc.pool.OracleDataSource;
 import java.sql.SQLException;
 
-<<<<<<<< HEAD:Game_Donation_Board/target/classes/context/Context_1_mybatis.java
 
 
-========
->>>>>>>> test:Game_Donation_Board/src/main/resources/context/Context_1_mybatis.java
 @Configuration
 public class Context_1_mybatis {
 
 	@Bean
-<<<<<<<< HEAD:Game_Donation_Board/target/classes/context/Context_1_mybatis.java
 	public DataSource ds()  throws SQLException{
 		 OracleDataSource ds = new OracleDataSource();
 	        ds.setURL("jdbc:oracle:thin:@project522_high?TNS_ADMIN=C:/DEV/backend_project/Wallet_Project522/");
@@ -30,27 +27,24 @@ public class Context_1_mybatis {
 //		ds.setUrl("jdbc:oracle:thin:@localhost:1521:xe");
 //		ds.setUsername("hr");
 //		ds.setPassword("hr");
-========
-	public DataSource ds() throws SQLException {
-		OracleDataSource ds = new OracleDataSource();
-		ds.setURL("jdbc:oracle:thin:@project522_high?TNS_ADMIN=C:/DEV/backend_project/Wallet_Project522/");
-		ds.setUser("ADMIN");
-		ds.setPassword("Multicampus522");
->>>>>>>> test:Game_Donation_Board/src/main/resources/context/Context_1_mybatis.java
 		return ds;
 	}
+	
 
+	
 	@Bean
-	public SqlSessionFactory factoryBean(DataSource ds) throws Exception {
+	public SqlSessionFactory factoryBean(DataSource ds) throws Exception{
 		SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
 		factoryBean.setDataSource(ds);
 
 		factoryBean.setConfigLocation(new ClassPathResource("config/mybatis/mybatis-config.xml"));
 		return factoryBean.getObject();
-	}
+	} 
+	
+	
 
 	@Bean
-	public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory factoryBean) {
+	public SqlSessionTemplate sqlSessionTemplate (SqlSessionFactory factoryBean) {
 		return new SqlSessionTemplate(factoryBean);
 	}
 }
